79b30487ecb29d8ec7c38f8bcd331286c8b484fa
krystiano13 <crystianotv@gmail.com>
2023-10-25 13:51:16 +0200

    create leave animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,36 @@
 import React from 'react';
 import gsap from 'gsap';
-import { Main } from './components/Universal/Main';
-import { HomeWrap } from "./components/Home/HomeWrap";
-import { Title } from "./components/Home/Title";
-import { HomeButton } from "./components/Home/HomeButton";
+import {Main} from './components/Universal/Main';
+import {HomeWrap} from "./components/Home/HomeWrap";
+import {Title} from "./components/Home/Title";
+import {HomeButton} from "./components/Home/HomeButton";
 
 const App = () => {
     const homeRef = React.useRef(null);
     const [render, setRender] = React.useState(false);
 
     React.useEffect(() => {
-        gsap.set(homeRef.current, { width : 0, scale : 0, autoAlpha : 0 });
-        gsap.to(homeRef.current,0.25,{ scale : 1, autoAlpha : 0.5 });
-        gsap.to(homeRef.current,0.25,{ width : 'auto', delay : 0.75, autoAlpha : 1 });
+        gsap.set(homeRef.current, {width: 0, scale: 0, autoAlpha: 0});
+        gsap.to(homeRef.current, 0.25, {scale: 1, autoAlpha: 0.5});
+        gsap.to(homeRef.current, 0.25, {width: 'auto', delay: 0.75, autoAlpha: 1});
     }, []);
+
+    const proceed = () => {
+        gsap.to(homeRef.current, 0.35, {width: 0, autoAlpha: 0.5});
+        gsap.to(homeRef.current, 0.35, {scale: 0, autoAlpha: 0, delay: 0.75});
+    }
 
     return (
         <Main>
             <HomeWrap ref={homeRef}>
                 <Title>
                     Dark Motivation
-                    <br />
-                    <HomeButton>Proceed</HomeButton>
+                    <br/>
+                    <HomeButton onClick={proceed}>Proceed</HomeButton>
                 </Title>
             </HomeWrap>
         </Main>
     );
 }
 
-export { App };
+export {App};
