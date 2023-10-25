70f034dc88bd0b6bb4c9ef1baab0efc7eed79138
krystiano13 <crystianotv@gmail.com>
2023-10-25 13:37:43 +0200

    create hover effect for button

--- src/components/Home/HomeButton.ts
+++ src/components/Home/HomeButton.ts
@@ -1,13 +1,41 @@
 import styled from 'styled-components';
 
 export const HomeButton = styled.button`
-  padding : 1rem;
-  border-radius: 2rem;
+  position: relative;
   background-color: transparent;
   color: whitesmoke;
   cursor: pointer;
-  padding-left : 2rem;
-  padding-right : 2rem;
   font-size : 1.25rem;
   border : solid whitesmoke 2px;
+  width : 25%;
+  height : 6vh;
+  min-width : 12rem;
+  min-height : 3.5rem;
+  transition: 250ms;
+  z-index: 1000;
+  
+  &::before {
+    content: '';
+    background-color: whitesmoke;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border-bottom-left-radius: 100%;
+    border-bottom-right-radius: 100%;
+    transition: transform 250ms, border-radius 250ms;
+    position: absolute;
+    z-index: -1;
+    transform: scaleY(0);
+    transform-origin: top;
+  }
+  
+  &:hover {
+    color : #0B0A0A;
+  }
+  
+  &:hover::before {
+    transform: scaleY(1);
+    border-radius: 0;
+  }
 `;
