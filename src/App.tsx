b5da11555a32ea9b8cd2f6d224d3e4a59dc3342f
krystiano13 <crystianotv@gmail.com>
2023-10-26 13:06:35 +0200

    implement loader component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Main } from './components/Universal/Main';
 import { Home } from './views/Home';
 import { Info } from "./views/Info";
+import {Loader} from "./components/Universal/Loader/Loader";
 
 const App = () => {
     return (
