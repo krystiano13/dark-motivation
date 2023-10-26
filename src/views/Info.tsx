f1e56b3d06c45e8d9a0feba2729c7fe64315cdfd
krystiano13 <crystianotv@gmail.com>
2023-10-26 13:02:50 +0200

    create outro animation for intro view

--- src/views/Info.tsx
+++ src/views/Info.tsx
@@ -3,19 +3,27 @@ import {HomeButton} from "../components/Home/HomeButton";
 import {InfoText} from "../components/Info/InfoText";
 
 import { useEffect, useRef } from "react";
+import { useNavigate } from "react-router";
 import gsap from 'gsap';
 
 const Info = () => {
     const textRef = useRef(null);
     const buttonRef = useRef(null);
+    const navigate = useNavigate();
 
     useEffect(() => {
         gsap.set(textRef.current, { autoAlpha : 0, y : 100 });
         gsap.set(buttonRef.current, { autoAlpha : 0, y : 100});
 
-        gsap.to(textRef.current, 0.35, { autoAlpha : 1, y : 0 });
-        gsap.to(buttonRef.current, 0.35, { autoAlpha : 1, y : 0, delay : 0.5 });
+        gsap.to(textRef.current, 0.35, { autoAlpha : 1, y : 0, delay : 0.25 });
+        gsap.to(buttonRef.current, 0.35, { autoAlpha : 1, y : 0, delay : 0.75 });
     },[]);
+
+    const handleNextStep = () => {
+        gsap.to(buttonRef.current, 0.35, { autoAlpha : 0, y : 100 });
+        gsap.to(textRef.current, 0.35, { autoAlpha : 0, y : 100, delay : 0.75 })
+            .then(() => {});
+    }
 
     return (
         <InfoWrap>
@@ -24,7 +32,7 @@ const Info = () => {
                 work towards your goals. As the name suggests it will do it in
                 a dark way. But before it, you'll need to answer few questions.
             </InfoText>
-            <HomeButton ref={buttonRef}>OK</HomeButton>
+            <HomeButton onClick={handleNextStep} ref={buttonRef}>OK</HomeButton>
         </InfoWrap>
     );
 }
