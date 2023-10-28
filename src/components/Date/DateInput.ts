f56007b7ee6e971bd14263c09ade146e53f39982
krystiano13 <crystianotv@gmail.com>
2023-10-28 11:32:28 +0200

    create basic styles for date input

--- src/components/Date/DateInput.ts
+++ src/components/Date/DateInput.ts
@@ -1,2 +1,10 @@
 import styled from 'styled-components';
-export const DateInput = styled.input``;
+export const DateInput = styled.input`
+  font-family: 'Montserrat', sans-serif;
+  outline: none;
+  padding : 1rem;
+  font-size : 1.4rem;
+  background-color: #0B0A0A;
+  color : whitesmoke;
+  border : solid whitesmoke 1px;
+`;
--- src/views/Date.tsx
+++ src/views/Date.tsx
@@ -1,9 +1,11 @@
 import {DateWrapper} from "../components/Date/DateWrapper";
 import {InfoText} from "../components/Info/InfoText";
+import {DateInput} from "../components/Date/DateInput";
 const Date = () => {
     return (
         <DateWrapper>
             <InfoText>Now. Choose your date of birth...</InfoText>
+            <DateInput type='date' />
         </DateWrapper>
     )
 }
