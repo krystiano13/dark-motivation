3f4ea55efe3aec7612b4001083d89570aa1b1c58
krystiano13 <crystianotv@gmail.com>
2023-10-28 12:13:27 +0200

    finish styles for date input

--- src/components/Date/DateInput.ts
+++ src/components/Date/DateInput.ts
@@ -2,9 +2,11 @@ import styled from 'styled-components';
 export const DateInput = styled.input`
   font-family: 'Montserrat', sans-serif;
   outline: none;
-  padding : 1rem;
+  box-sizing: border-box;
+  width : 14rem;
   font-size : 1.4rem;
   background-color: #0B0A0A;
   color : whitesmoke;
   border : solid whitesmoke 1px;
+  padding : 0.5rem;
 `;
