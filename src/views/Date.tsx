12df76049302f0534aa14d753c90670a4535b3ff
krystiano13 <crystianotv@gmail.com>
2023-10-28 12:16:22 +0200

    create appear animation in date view

--- src/views/Date.tsx
+++ src/views/Date.tsx
@@ -1,11 +1,24 @@
 import {DateWrapper} from "../components/Date/DateWrapper";
 import {InfoText} from "../components/Info/InfoText";
 import {DateInput} from "../components/Date/DateInput";
+
+import gsap from 'gsap';
+import { useEffect, useRef } from "react";
+
 const Date = () => {
+    const text = useRef(null);
+    const date = useRef(null);
+
+    useEffect(() => {
+        gsap.set([text.current, date.current], { y : 100, autoAlpha : 0 });
+        gsap.to(text.current, 0.35, { y : 0, autoAlpha : 1, delay : 0.2 });
+        gsap.to(date.current, 0.35, { y : 0, autoAlpha : 1, delay : 0.6 })
+    }, []);
+
     return (
         <DateWrapper>
-            <InfoText>Now. Choose your date of birth...</InfoText>
-            <DateInput type='date' />
+            <InfoText ref={text}>Now. Choose your date of birth...</InfoText>
+            <DateInput ref={date} type='date' />
         </DateWrapper>
     )
 }
